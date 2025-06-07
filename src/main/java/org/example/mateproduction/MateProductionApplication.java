b9632babac47b3ca03a3b4be6f04324b6c36fdd5
Aidyn <nurlan.aydin06@gmail.com>
2025-06-07 19:12:11 +0500

    Fixed error on maven with version

--- src/main/java/org/example/mateproduction/MateProductionApplication.java
+++ src/main/java/org/example/mateproduction/MateProductionApplication.java
@@ -3,7 +3,6 @@ package org.example.mateproduction;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
 public class MateProductionApplication {
 
     public static void main(String[] args) {
