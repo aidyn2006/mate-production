86e6ea1d4f390d9f47bf12d017290947297d47bb
Aidyn <nurlan.aydin06@gmail.com>
2025-06-07 19:35:58 +0500

    StreetService

--- src/main/java/org/example/mateproduction/service/StreetService.java
+++ src/main/java/org/example/mateproduction/service/StreetService.java
@@ -1,4 +1,18 @@
 package org.example.mateproduction.service;
 
+import org.example.mateproduction.dto.request.StreetRequest;
+import org.example.mateproduction.dto.response.StreetResponse;
+
+import java.util.List;
+import java.util.UUID;
+
 public interface StreetService {
+
+    StreetResponse createStreet(StreetRequest request);
+    List<StreetResponse> getAllStreet();
+
+    StreetResponse getStreetByCity(UUID cityId);
+
+    StreetResponse updateStreet(UUID streetId,StreetRequest request);
+    void deleteStreet(UUID streetId);
 }
