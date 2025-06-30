7c4082421afe6a3e15cd501e43a119636c55b518
Aidyn <nurlan.aydin06@gmail.com>
2025-06-30 17:46:20 +0500

    oAuth2 done included customOauth and Succesfull handler

--- src/main/java/org/example/mateproduction/service/impl/OAuth2LoginSuccessHandler.java
+++ src/main/java/org/example/mateproduction/service/impl/OAuth2LoginSuccessHandler.java
@@ -7,6 +7,8 @@ import org.example.mateproduction.config.Jwt.JwtService;
 import org.example.mateproduction.config.Jwt.JwtUserDetails;
 import org.example.mateproduction.entity.User;
 import org.example.mateproduction.repository.UserRepository;
+import org.example.mateproduction.util.Role;
+import org.example.mateproduction.util.UserStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
@@ -27,14 +29,30 @@ public class OAuth2LoginSuccessHandler implements AuthenticationSuccessHandler {
     public void onAuthenticationSuccess(HttpServletRequest request,
                                         HttpServletResponse response,
                                         Authentication authentication) throws IOException {
-
         OAuth2User oauthUser = (OAuth2User) authentication.getPrincipal();
-        String email = oauthUser.getAttribute("email");
 
-        User user = userRepository.findByEmail(email).orElseThrow();
+        String email = oauthUser.getAttribute("email");
+        String name = oauthUser.getAttribute("given_name");
+        String surname = oauthUser.getAttribute("family_name");
+        String picture = oauthUser.getAttribute("picture");
+
+        User user = userRepository.findByEmail(email)
+                .orElseGet(() -> {
+                    User newUser = User.builder()
+                            .email(email)
+                            .username(email)
+                            .name(name)
+                            .surname(surname)
+                            .avatarUrl(picture)
+                            .isVerified(true)
+                            .status(UserStatus.ACTIVE)
+                            .role(Role.USER)
+                            .build();
+                    return userRepository.save(newUser);
+                });
+
         String token = jwtService.generateToken(new JwtUserDetails(user));
 
-        String redirectUrl = "http://localhost:3000/oauth-success?token=" + token;
-        response.sendRedirect(redirectUrl);
+        response.sendRedirect("http://localhost:3000/oauth-success?token=" + token);
     }
 }
