d268d8cb22f8b5d93e6255dd67f74591e99eb9da
Aidyn <nurlan.aydin06@gmail.com>
2025-07-04 13:09:25 +0500

    Deleted corsConfig

--- src/main/java/org/example/mateproduction/config/Jwt/CorsConfig.java
+++ src/main/java/org/example/mateproduction/config/Jwt/CorsConfig.java
@@ -21,17 +21,17 @@ public class CorsConfig {
 //            }
 //        };
 //    }
-@Bean
-public WebMvcConfigurer corsConfigurer() {
-    return new WebMvcConfigurer() {
-        @Override
-        public void addCorsMappings(CorsRegistry registry) {
-            registry.addMapping("/**")
-                    .allowedOriginPatterns("*") // Разрешает ВСЕ домены
-                    .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                    .allowedHeaders("*")
-                    .allowCredentials(true);
-        }
-    };
-}
+//@Bean
+//public WebMvcConfigurer corsConfigurer() {
+//    return new WebMvcConfigurer() {
+//        @Override
+//        public void addCorsMappings(CorsRegistry registry) {
+//            registry.addMapping("/**")
+//                    .allowedOriginPatterns("*") // Разрешает ВСЕ домены
+//                    .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+//                    .allowedHeaders("*")
+//                    .allowCredentials(true);
+//        }
+//    };
+//}
 }
