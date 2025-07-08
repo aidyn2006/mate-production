e4abde7dc6a01c407780786c7e47eb283f67112e
a.nurlan <a.nurlan@documentolog.com>
2025-07-08 17:41:42 +0500

    Optimizing emailservice

--- src/main/java/org/example/mateproduction/service/impl/EmailServiceImpl.java
+++ src/main/java/org/example/mateproduction/service/impl/EmailServiceImpl.java
@@ -5,6 +5,7 @@ import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.example.mateproduction.service.EmailService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.scheduling.annotation.Async;
@@ -17,21 +18,24 @@ public class EmailServiceImpl implements EmailService {
 
     private final JavaMailSender mailSender;
 
+    @Value("${email.from}")
+    private String fromEmail;
+
     @Override
-    @Async // To avoid blocking the main thread
+    @Async
     public void sendEmail(String to, String subject, String email) {
         try {
             MimeMessage mimeMessage = mailSender.createMimeMessage();
             MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, "utf-8");
-            helper.setText(email, true); // true indicates HTML
+            helper.setText(email, true);
             helper.setTo(to);
             helper.setSubject(subject);
-            helper.setFrom("suhansun13@gmail.com"); // Set your from address
+            helper.setFrom(fromEmail);
             mailSender.send(mimeMessage);
             log.info("Email sent successfully to {}", to);
         } catch (MessagingException e) {
             log.error("Failed to send email to {}", to, e);
-            throw new IllegalStateException("Failed to send email");
+            throw new IllegalStateException("Failed to send email", e);
         }
     }
 }
